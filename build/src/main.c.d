62e6719e6aa2ef20613ac9e31980852713fa2a6b
Jeeli <elihantman@gmail.com>
2024-01-09 22:06:14 -0800

    Most of the graphics pipeline setup is done, I hope All thats left should be render passes, and other shit

--- build/src/main.c.d
+++ build/src/main.c.d
@@ -1,12 +1,14 @@
-build/./src/main.c.o: src/main.c src/Vulkan.h src/error.h src/instance.h \
- src/optional.h src/physicaldevice.h src/logicaldevice.h src/swapchain.h \
- src/lib/SimpleMath/src/include/misc.h \
+build/./src/main.c.o: src/main.c src/Vulkan.h src/error.h src/util.h \
+ src/instance.h src/optional.h src/physicaldevice.h src/logicaldevice.h \
+ src/swapchain.h src/lib/SimpleMath/src/include/misc.h \
  src/lib/SimpleMath/src/include/misc/sqrt.h \
  src/lib/SimpleMath/src/include/misc/clamp.h src/imageviews.h
 
 src/Vulkan.h:
 
 src/error.h:
+
+src/util.h:
 
 src/instance.h:
 
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "Vulkan.h"
 #include "error.h"
+#include "util.h"
 #include "instance.h"
 #include "optional.h"
 #include "physicaldevice.h"
@@ -19,47 +20,184 @@
 static const uint32_t WIDTH = 1920;
 static const uint32_t HEIGHT = 1080;
 
-typedef struct SizedBuffer {
-    size_t size;
-    char* buffer;
-} SizedBuffer;
+typedef struct PipelineData {
+    VkViewport view;
+    VkRect2D scissor;
+    VkPipelineLayout layout;
+} PipelineData;
 
-// Buffer is allocated with malloc, must be manually freed
-static void LoadFile(const char* filename, SizedBuffer* buffer) {
+
+static void CreateGraphicsPipeline(VkDevice logicalDevice, SwapImageDetails swapChainExtent, PipelineData* pipeData, GLFWwindow* window) {
+    //Load Shaders
+    SizedBuffer vert; 
+    SizedBuffer frag;
     errno = 0;
-    FILE* file = fopen(filename, "rb"); 
-    if (file == NULL) {
-        fprintf(stderr, ERR_COLOR("Failed to Open File"));
-        return;
-    }
-
-    fseek(file, 0, SEEK_END);
-    buffer->size = (size_t)ftell(file);
-    fseek(file, 0, SEEK_SET); 
-
-    buffer->buffer = (char *)aligned_alloc(sizeof(uint32_t), buffer->size*sizeof(char));
-
-
-    if (buffer == NULL) {
-        fprintf(stderr, ERR_COLOR("Malloc failed somehow. Fuck"));
-        fclose(file);
-        return;
-    }
-
-    if (fread(buffer->buffer, sizeof(char), buffer->size, file) != buffer->size) {
-        fprintf(stderr, ERR_COLOR("Failed to Read entire file"));
-        fclose(file);
-        return;
-    }
+    LoadFile(".//shaders//standard.vert.spv", &vert);
+    LoadFile(".//shaders//standard.frag.spv", &frag);
 
     if (errno != 0) {
-        perror("readfile func");
+        errno = FailedSearch;
+        fprintf(stderr, ERR_COLOR("Failed to Load File"));
+        return;
     }
 
-    fclose(file);
-    return;
-}
 
+    VkShaderModuleCreateInfo VertCreateInfo = {0};
+    VertCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    VertCreateInfo.codeSize = vert.size;
+    VertCreateInfo.pCode = (uint32_t*)vert.buffer;
+
+    VkShaderModule vertex;
+    if (vkCreateShaderModule(logicalDevice, &VertCreateInfo, NULL, &vertex) != VK_SUCCESS) {
+        free(vert.buffer);
+        free(frag.buffer);
+        errno = FailedCreation;
+        fprintf(stderr, ERR_COLOR("Failed to Create Vertex Shader Module"));
+        return;
+    }
+    free(vert.buffer);
+
+    VkShaderModuleCreateInfo FragCreateInfo = {0};
+    FragCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    FragCreateInfo.codeSize = frag.size;
+    FragCreateInfo.pCode = (uint32_t*)frag.buffer;
+
+    VkShaderModule fragment;
+    if (vkCreateShaderModule(logicalDevice, &FragCreateInfo, NULL, &fragment) != VK_SUCCESS) {
+        free(frag.buffer);
+        vkDestroyShaderModule(logicalDevice, vertex, NULL);
+        errno = FailedCreation;
+        fprintf(stderr, ERR_COLOR("Failed to Create Fragment Shader Module"));
+        return;
+
+    }
+    free(frag.buffer);
+
+    VkPipelineShaderStageCreateInfo vertStageInfo = {0};
+    vertStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertStageInfo.module = vertex;
+    vertStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragStageInfo = {0};
+    fragStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragStageInfo.module = fragment;
+    fragStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertStageInfo, fragStageInfo};
+
+    //fixed shader stages
+    const VkDynamicState dynamicStates[] = {
+//        VK_DYNAMIC_STATE_VIEWPORT,
+//        VK_DYNAMIC_STATE_SCISSOR,
+    };
+    
+    VkPipelineDynamicStateCreateInfo dynamicCreateInfo = {0};
+    dynamicCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+    dynamicCreateInfo.dynamicStateCount = sizeof(dynamicStates)/sizeof(dynamicStates[0]); 
+    dynamicCreateInfo.pDynamicStates = dynamicStates;
+
+
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {0};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.pVertexBindingDescriptions = NULL;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+    vertexInputInfo.pVertexAttributeDescriptions = NULL;
+
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo = {0};
+    inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
+
+
+    VkViewport viewport = {0};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = (float)swapChainExtent.extent.width;
+    viewport.height = (float)swapChainExtent.extent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D scissor = {0};
+    scissor.offset = (VkOffset2D){0,0};
+    scissor.extent = swapChainExtent.extent;
+
+    VkPipelineViewportStateCreateInfo viewportInfo = {0};
+    viewportInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportInfo.viewportCount = 1;
+    viewportInfo.pViewports = &viewport;
+    viewportInfo.scissorCount = 1;
+    viewportInfo.pScissors = &scissor;
+
+
+    VkPipelineRasterizationStateCreateInfo rasterizerInfo = {0};
+    rasterizerInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizerInfo.depthClampEnable = VK_FALSE;
+    rasterizerInfo.rasterizerDiscardEnable = VK_TRUE;
+    rasterizerInfo.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizerInfo.lineWidth = 1.0f;
+    rasterizerInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizerInfo.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizerInfo.depthBiasEnable = VK_FALSE;
+    rasterizerInfo.depthBiasConstantFactor = 0.0f;
+    rasterizerInfo.depthBiasClamp = 0.0f;
+    rasterizerInfo.depthBiasSlopeFactor = 0.0f;
+
+    VkPipelineMultisampleStateCreateInfo multisamplingInfo = {0};
+    multisamplingInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisamplingInfo.sampleShadingEnable = VK_FALSE;
+    multisamplingInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisamplingInfo.minSampleShading = 1.0f; 
+    multisamplingInfo.pSampleMask = NULL; 
+    multisamplingInfo.alphaToCoverageEnable = VK_FALSE; 
+    multisamplingInfo.alphaToOneEnable = VK_FALSE; 
+
+
+    VkPipelineColorBlendAttachmentState colorBlendInfo = {0};
+    colorBlendInfo.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    colorBlendInfo.blendEnable = VK_FALSE;
+    colorBlendInfo.srcColorBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+    colorBlendInfo.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+    colorBlendInfo.colorBlendOp = VK_BLEND_OP_ADD; // Optional
+    colorBlendInfo.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE; // Optional
+    colorBlendInfo.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO; // Optional
+    colorBlendInfo.alphaBlendOp = VK_BLEND_OP_ADD; // Optional
+    
+    VkPipelineColorBlendStateCreateInfo colorStateInfo = {0};
+    colorStateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colorStateInfo.logicOpEnable = VK_FALSE;
+    colorStateInfo.logicOp = VK_LOGIC_OP_COPY; 
+    colorStateInfo.attachmentCount = 1;
+    colorStateInfo.pAttachments = &colorBlendInfo;
+    colorStateInfo.blendConstants[0] = 0.0f; 
+    colorStateInfo.blendConstants[1] = 0.0f; 
+    colorStateInfo.blendConstants[2] = 0.0f; 
+    colorStateInfo.blendConstants[3] = 0.0f; 
+
+    VkPipelineLayoutCreateInfo layoutInfo = {0};
+    layoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    layoutInfo.setLayoutCount = 0; 
+    layoutInfo.pSetLayouts = NULL; 
+    layoutInfo.pushConstantRangeCount = 0; 
+    layoutInfo.pPushConstantRanges = NULL; 
+
+    if (vkCreatePipelineLayout(logicalDevice, &layoutInfo, NULL, &pipeData->layout) != VK_SUCCESS) {
+        errno = FailedCreation;
+        fprintf(stderr, ERR_COLOR("Failed to create Pipeline Layout"));
+        vkDestroyShaderModule(logicalDevice, vertex, NULL);
+        vkDestroyShaderModule(logicalDevice, fragment, NULL);
+        return;
+    }
+    fprintf(stdout, TRACE_COLOR("\tPipeline Layout Created"));
+
+
+
+    vkDestroyShaderModule(logicalDevice, vertex, NULL);
+    vkDestroyShaderModule(logicalDevice, fragment, NULL);
+    fprintf(stdout, TRACE_COLOR("Created Graphics Pipeline"));
+}
 
 
 
@@ -180,15 +318,10 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-
-    //Load Shaders
-    SizedBuffer vert; 
-    SizedBuffer frag;
-    errno = 0;
-    LoadFile(".//shaders//standard.vert.spv", &vert);
-    LoadFile(".//shaders//standard.frag.spv", &frag);
-
-    if (errno != 0) {
+    //create Graphics Pipeline
+    PipelineData data;
+    CreateGraphicsPipeline(logicalDevice, details, &data, window);
+    if (errno == FailedCreation) {
         CloseGLFW(window);
         if (enableValidationLayers)  DestroyDebugMessenger(&instance, &debugMessenger);
         DestroyImageViews(logicalDevice, swapViews, imageCount);
@@ -199,50 +332,6 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-
-    VkShaderModuleCreateInfo VertCreateInfo = {0};
-    VertCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    VertCreateInfo.codeSize = vert.size;
-    VertCreateInfo.pCode = (uint32_t*)vert.buffer;
-
-    VkShaderModule vertex;
-    if (vkCreateShaderModule(logicalDevice, &VertCreateInfo, NULL, &vertex) != VK_SUCCESS) {
-        free(vert.buffer);
-        free(frag.buffer);
-
-        CloseGLFW(window);
-        if (enableValidationLayers)  DestroyDebugMessenger(&instance, &debugMessenger);
-        DestroyImageViews(logicalDevice, swapViews, imageCount);
-        vkDestroySwapchainKHR(logicalDevice, swapchain, NULL);
-        vkDestroySurfaceKHR(instance, surface, NULL);
-        vkDestroyDevice(logicalDevice, NULL);
-        vkDestroyInstance(instance, NULL);
-        exit(EXIT_FAILURE);
-    }
-    free(vert.buffer);
-
-    VkShaderModuleCreateInfo FragCreateInfo = {0};
-    FragCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    FragCreateInfo.codeSize = frag.size;
-    FragCreateInfo.pCode = (uint32_t*)frag.buffer;
-
-    VkShaderModule fragment;
-    if (vkCreateShaderModule(logicalDevice, &FragCreateInfo, NULL, &fragment) != VK_SUCCESS) {
-        free(frag.buffer);
-
-        CloseGLFW(window);
-        if (enableValidationLayers)  DestroyDebugMessenger(&instance, &debugMessenger);
-        DestroyImageViews(logicalDevice, swapViews, imageCount);
-        vkDestroySwapchainKHR(logicalDevice, swapchain, NULL);
-        vkDestroySurfaceKHR(instance, surface, NULL);
-        vkDestroyDevice(logicalDevice, NULL);
-        vkDestroyInstance(instance, NULL);
-        exit(EXIT_FAILURE);
-    }
-    free(frag.buffer);
-
-    vkDestroyShaderModule(logicalDevice, vertex, NULL);
-    vkDestroyShaderModule(logicalDevice, fragment, NULL);
 
     
 
@@ -256,6 +345,7 @@ int main() {
     if (enableValidationLayers)  DestroyDebugMessenger(&instance, &debugMessenger);
     
 
+    vkDestroyPipelineLayout(logicalDevice, data.layout, NULL);
     DestroyImageViews(logicalDevice, swapViews, imageCount);
     vkDestroySwapchainKHR(logicalDevice, swapchain, NULL);
     vkDestroySurfaceKHR(instance, surface, NULL);
